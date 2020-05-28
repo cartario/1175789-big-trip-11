9d23af184974bc3013b8bf63ba75595450307fa0
Vasiliy <cartario@yandex.ru>
2020-05-28 15:51:27 +0300

    LocalStorage

---
+++ src/api/provider.js
@@ -0,0 +1,115 @@
+import Point from "../models/point.js";
+import {nanoid} from "nanoid";
+
+const isOnline = () => {
+  return window.navigator.onLine;
+};
+
+const getSyncedPoints = (items) => {
+  return items.filter(({success}) => success)
+    .map(({payload}) => payload.point);
+};
+
+const createStoreStructure = (items) => {
+  return items.reduce((acc, current) => {
+    return Object.assign({}, acc, {
+      [current.id]: current,
+    });
+  }, {});
+};
+
+export default class Provider {
+  constructor(api, store) {
+    this._api = api;
+    this._store = store;
+  }
+
+  getEvents() {
+    if (isOnline()) {
+      return this._api.getEvents()
+        .then((points) => {
+          const items = createStoreStructure(points.map((point) => point.toRAW()));
+
+          this._store.setItems(items);
+          return points;
+        });
+    }
+    const storePoints = Object.values(this._store.getItems());
+    return Promise.resolve(Point.parsePoints(storePoints));
+  }
+
+  updateEvent(id, data) {
+    if (isOnline()) {
+      return this._api.updateEvent(id, data)
+        .then((newEvent) => {
+          this._store.setItem(newEvent.id, newEvent.toRAW());
+          return newEvent;
+        });
+    }
+    const localEvent = Point.clone(Object.assign(data, {id}));
+    this._store.setItem(id, localEvent.toRAW());
+    return Promise.resolve(localEvent);
+  }
+
+  createEvent(event) {
+    if (isOnline()) {
+      return this._api.createEvent(event)
+        .then((newEvent) => {
+          this._store.setItem(newEvent.id, newEvent.toRAW());
+          return newEvent;
+        });
+    }
+
+    const localNewEventId = nanoid();
+    const localNewEvent = Point.clone(Object.assign(event, {id: localNewEventId}));
+    this._store.setItem(localNewEvent.id, localNewEvent.toRAW());
+    return Promise.resolve(localNewEvent);
+  }
+
+  deleteEvent(id) {
+    if (isOnline()) {
+      return this._api.deleteEvent(id)
+        .then(() => this._store.removeItem(id));
+    }
+
+    this._store.removeItem(id);
+    return Promise.resolve();
+  }
+
+  getOffers() {
+    if (isOnline()) {
+      return this._api.getOffers();
+    }
+
+    return Promise.resolve();
+  }
+
+  getDestinations() {
+    if (isOnline()) {
+      return this._api.getDestinations();
+    }
+
+    return Promise.resolve();
+  }
+
+  sync() {
+    if (isOnline()) {
+      const storePoints = Object.values(this._store.getItems());
+
+      return this._api.sync(storePoints)
+        .then((response) => {
+          // Забираем из ответа синхронизированные задачи
+          const createdPoints = getSyncedPoints(response.created);
+          const updatedPoints = getSyncedPoints(response.updated);
+
+          // Добавляем синхронизированные задачи в хранилище.
+          // Хранилище должно быть актуальным в любой момент.
+          const items = createStoreStructure([...createdPoints, ...updatedPoints]);
+
+          this._store.setItems(items);
+        });
+    }
+
+    return Promise.reject(new Error(`Sync data failed`));
+  }
+}
